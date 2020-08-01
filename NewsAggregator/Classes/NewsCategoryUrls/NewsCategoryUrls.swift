a7d8a71a799c825071afb6e289c02b810ce42bcc
Nikita <berezinsky.nikita@gmail.com>
2020-08-02 01:57:12 +0300

    Update NewsCategoryUrls.swift

--- NewsAggregator/Classes/NewsCategoryUrls/NewsCategoryUrls.swift
+++ NewsAggregator/Classes/NewsCategoryUrls/NewsCategoryUrls.swift
@@ -7,7 +7,4 @@ struct NewsCategoryUrls {
         case bitcoin = "http://newsapi.org/v2/everything?q=bitcoin&from=2020-06-15&sortBy=publishedAt&apiKey=8e4bfcbe5d0243448601ad3d3370e19f"
         case techcrunch = "http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=8e4bfcbe5d0243448601ad3d3370e19f"
     }
-    
-    
-    
 }
