caf62ac74f2d09e48c717319e8877360862e35d4
Nikita <berezinsky.nikita@gmail.com>
2020-08-01 23:49:28 +0300

    Initial Commit

---
+++ NewsAggregator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsAggregator
+//
+//  Created by fab1zon on 8/1/20.
+//  Copyright © 2020 Никита Березинский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
